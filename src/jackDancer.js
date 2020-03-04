71d3c8054418a0d5c8272b62d660e35ce4c487a9
Micko <mickocabacungan@gmail.com>
2020-03-04 13:59:58 -0800

    Complete adding css

--- src/jackDancer.js
+++ src/jackDancer.js
@@ -1,6 +1,6 @@
 var jackDancer = function(top, left, timeBetweenSteps){
   makeDancer.call(this, top, left, timeBetweenSteps);
-  this.$node = $('<img class="dynamite" src="https://media0.giphy.com/media/dQpqkxXyPvb2iImius/giphy.gif">');
+  this.$node = $('<img class="jack" src="https://media0.giphy.com/media/dQpqkxXyPvb2iImius/giphy.gif">');
   this.oldStep = makeDancer.prototype.step;
 
 }
